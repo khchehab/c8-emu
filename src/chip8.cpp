024d79a3f9a5fa91cfe643d7bea4e7c580506a06
khaled.chehab <khchehab@gmail.com>
2024-04-15 18:07:57 +0300

    removed old commented code

--- src/chip8.cpp
+++ src/chip8.cpp
@@ -270,34 +270,6 @@ void Chip8::drawSprite(uint8_t x, uint8_t y, uint8_t n) {
             }
         }
     }
-
-//    V[0xf] = 0x0;
-//
-//    uint8_t i, j, sprite;
-//    uint8_t xPos, yPos;
-//    uint8_t spritePixel;
-//    uint16_t index;
-//    bool screenPixel;
-//    for (i = 0; i < n; ++i) {
-//        sprite = memory[I + i];
-//
-//        for (j = 0; j < 8; ++j) {
-//            xPos = (V[x] + j) % GRAPHICS_WIDTH;
-//            yPos = (V[y] + i) % GRAPHICS_HEIGHT;
-//            index = (yPos * GRAPHICS_WIDTH) + xPos;
-//
-//            spritePixel = (sprite & (0x80 >> j)) >> (8 - j - 1);
-//            screenPixel = graphics[index];
-//
-//            if (spritePixel == 0x1) {
-//                if (screenPixel) {
-//                    V[0xf] = 0x1;
-//                }
-//
-//                graphics[index] = screenPixel ^ spritePixel;
-//            }
-//        }
-//    }
 }
 
 const bool *Chip8::getGraphics() const {
